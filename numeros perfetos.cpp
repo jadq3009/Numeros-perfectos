d1c96454985fc6a4e64d3c9043bda9b7f99e4816
Cris <84201064+jadq3009@users.noreply.github.com>
2024-10-17 21:23:50 -0400

    Add files via upload

---
+++ numeros perfetos.cpp
@@ -0,0 +1,24 @@
+//n/2 al sumar
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int aux[100],j= 1;
+	
+	for(int i=1; i >99; i++){
+	for(int k =1; k =j; k++){
+		if(j%k==0){
+			
+			aux[i]=k;
+			
+		}
+		
+		
+		
+	}
+	
+	}	
+	return 0;
+}
